71fed92dc244dac7261d65e0be4dae0d9679fd39
Artjom Kurapov <artkurapov@gmail.com>
2024-09-21 02:30:02 +0300

    Update restart.sh
    Signed-off-by: Artjom Kurapov <artkurapov@gmail.com>

--- restart.sh
+++ restart.sh
@@ -3,5 +3,7 @@ cd /www/models-bee-detector/
 chmod 777 tmp
 chmod 755 weights
 
+rm -rf /www/models-bee-detector/tmp/*
+
 COMPOSE_PROJECT_NAME=gratheon docker-compose down
 COMPOSE_PROJECT_NAME=gratheon docker-compose up -d --build
